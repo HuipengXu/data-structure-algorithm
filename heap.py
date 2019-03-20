c9b75881a2c304683ddc050824d07d6eb810e382
Brycexxx <2531094755@qq.com>
2019-03-20 19:14:36 +0800

    为 dijkstra 添加小顶堆实现

--- heap.py
+++ heap.py
@@ -2,6 +2,9 @@
 # @Author  : Xu Huipeng
 # @Blog    : https://brycexxx.github.io/
 
+from typing import List, Optional, Tuple
+
+
 class BigTopHeap:
 
     def __init__(self, arr: list):
@@ -60,6 +63,65 @@ class BigTopHeap:
         for i in range(1, self.length - 1):
             self._delete_top(self.length - i)
         return self.heap[1:]
+
+
+class SmallTopHeap:
+    """
+    为 dijkstra 服务
+    """
+
+    def __init__(self):
+        self.heap: List[Optional[list]] = [None]
+        self.length = 1
+
+    def _up_heapify(self, idx: int, val: list):
+        """
+        从下往上堆化
+        """
+        while True:
+            parent_idx = idx // 2
+            if parent_idx == 0 or self.heap[parent_idx][1] <= val[1]:
+                break
+            self.heap[parent_idx], self.heap[idx] = self.heap[idx], self.heap[parent_idx]
+            idx = parent_idx
+
+    def insert(self, val: list):
+        self.heap.append(val)
+        self._up_heapify(self.length, val)
+        self.length += 1
+
+    def poll(self) -> Optional[list]:
+        if self.length == 1: return None
+        self.heap[1], self.heap[self.length - 1] = self.heap[self.length - 1], self.heap[1]
+        vertex = self.heap.pop()
+        self.length -= 1
+        idx = 1
+        # 从上往下堆化
+        while True:
+            left, right = 2 * idx, 2 * idx + 1
+            if left >= self.length:
+                break
+            elif right >= self.length or self.heap[left][1] <= self.heap[right][1]:
+                swap_idx = left
+            else:
+                swap_idx = right
+            if self.heap[idx][1] < self.heap[swap_idx][1]:
+                break
+            self.heap[idx], self.heap[swap_idx] = self.heap[swap_idx], self.heap[idx]
+            idx = swap_idx
+        return vertex
+
+    def update(self, val: list) -> bool:
+        for i in range(1, self.length):
+            if self.heap[i][0] == val[0]:
+                self.heap[i][1] = val[1]
+                self._up_heapify(i, val)
+                return True
+        return False
+
+    def is_empty(self) -> bool:
+        if self.length == 1: return True
+        return False
 
 
 if __name__ == "__main__":
