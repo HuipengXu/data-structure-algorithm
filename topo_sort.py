85157dd21e9628dc1111bec90850551768d63468
Brycexxx <2531094755@qq.com>
2019-03-21 15:34:18 +0800

    基于 dfs 的拓扑排序

--- topo_sort.py
+++ topo_sort.py
@@ -3,6 +3,7 @@
 # @Blog    : https://brycexxx.github.io/
 
 from dijkstra import DirectedGraph
+from single_linked_list import SingleLinkedList
 
 
 def topo_sort_by_kahn(dg: DirectedGraph):
@@ -32,10 +33,43 @@ def topo_sort_by_kahn(dg: DirectedGraph):
             node = node._next
 
 
+def topo_sort_by_dfs(dg: DirectedGraph):
+    adj = dg.adj
+    vertex_nums = len(adj)
+    visited = [False] * vertex_nums
+    reverse_adj = [SingleLinkedList() for _ in range(vertex_nums)]
+
+    # 构造逆邻接表
+    for i in range(vertex_nums):
+        v = adj[i].head
+        while v:
+            if v.val is None: break
+            id_ = v.val[0]
+            reverse_adj[id_].append((i, 1))
+            v = v._next
+
+    def dfs(i):
+        v = reverse_adj[i].head
+        while v:
+            if v.val is None or visited[v.val[0]]: break
+            id_ = v.val[0]
+            visited[id_] = True
+            dfs(id_)
+            v = v._next
+        print(i)
+
+    # 遍历
+    for i in range(vertex_nums):
+        if visited[i]: continue
+        visited[i] = True
+        dfs(i)
+
+
 if __name__ == "__main__":
     dt = {'内裤': 0, '裤子': 1, '腰带': 2, '袜子': 3, '鞋子': 4, '衬衣': 5, '领带': 6, '外套': 7}
     edges = [(0, 1, 1), (0, 4, 1), (1, 4, 1), (1, 2, 1), (3, 4, 1), (5, 7, 1), (5, 6, 1)]
     dg = DirectedGraph(8)
     for e in edges:
         dg.add_edge(*e)
-    topo_sort_by_kahn(dg)
+    # topo_sort_by_kahn(dg)
+    topo_sort_by_dfs(dg)
